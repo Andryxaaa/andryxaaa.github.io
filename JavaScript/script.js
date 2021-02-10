39f9b881b7ad3fd5822b270fba76b5382f925370
Andryxaaa <54295843+Andryxaaa@users.noreply.github.com>
2021-02-10 19:14:34 +0300

    Delete JavaScript directory

--- JavaScript/script.js
+++
@@ -1,6 +0,0 @@
-
-        const status = document.querySelector('.status').querySelector('h1');
-        const redDivs = document.querySelectorAll('#red');
-        
-        status.textContent += redDivs.length;
-            
